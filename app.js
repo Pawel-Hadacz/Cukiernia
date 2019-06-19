7c03ae843900cb5d27d338a11e43ca5745d470c5
R1serX <pawel.hadacz96@gmail.com>
2019-06-19 13:29:54 +0200

    inint-parser

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const http = require('http');
+
 //const fs = require('rs');
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
 
-const server =  http.createServer(app);
-  
+app.use(bodyparser.urlencoded());
 
-server.listen(3000);
+app.use('/add-creams',(req,res,next)=>{
+    res.send('<form action="/creams" method="POST"><input type="text" name="nazwa"><button type="submit">Dodaj smak</button></input></form>')
+    });
+app.use('/creams',(req,res)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/',(req,res,next)=>{
+   res.send('<p>strona glowna</p>')
+    });
+
+app.listen(3000);
