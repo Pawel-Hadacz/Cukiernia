0b99972b81c159a04f1e130db32941a56d7a15c4
R1serX <pawel.hadacz96@gmail.com>
2019-06-19 03:09:29 +0200

    przygotowanie

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
+//const fs = require('rs');
+const express = require('express');
+const app = express();
 
-
-const server =  http.createServer((req,res)=>{
-    console.log(req);
-    process.exit();
-});
+const server =  http.createServer(app);
+  
 
 server.listen(3000);
